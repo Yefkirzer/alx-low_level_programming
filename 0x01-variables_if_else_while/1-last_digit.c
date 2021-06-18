0f318c15bcbba0d6b98dee805cdd72061db70307
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-18 11:08:18 -0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ else if (l == 0)
 {
 printf("the last digit of %d is %d and is 0\n", n, l);
 }
-else if(l > 6 && l!=0)
+else
 {
 printf("the last digit of %d is %d and and is less than 6 and not 0\n", n, l);
 }
