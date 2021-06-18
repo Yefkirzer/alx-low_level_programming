cb37a1ab8db95b2a9cf154dc0deeb4972bbae762
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-18 12:22:32 -0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 int c, i, k, j;
-
 for (c = 48; c <= 57; c++)
 {
 for (i = 48; i <= 57; i++)
@@ -24,7 +23,6 @@ putchar(i);
 putchar(' ');
 putchar(k);
 putchar(j);
-
 if (c + i + k + j == 227 && c == 57)
 {
 break;
@@ -39,7 +37,6 @@ putchar(' ');
 }
 }
 }
-
 putchar('\n');
 return (0);
 }       
