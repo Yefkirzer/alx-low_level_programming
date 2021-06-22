b32f5f944f19fa7f6c4f2b749f3c3894294ddf16
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-22 11:49:34 -0300

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -12,22 +11,21 @@
 
 int main(void)
 {
-  long int a, b, c, d;
-
-  a = 0;
-  b = 1;
-  c = 0;
-  d = 0;
-  while (1)
-    {
-      c = a + b;
-      a = b;
-      b = c;
-      if (c > 4000000)
-	break;
-      if ((c % 2) == 0)
-	d += c;
-    }
-  printf("%ld\n", d);
-  return (0);
+long int a, b, c, d;
+a = 0;
+b = 1;
+c = 0;
+d = 0;
+while (1)
+{
+c = a + b;
+a = b;
+b = c;
+if (c > 4000000)
+break;
+if ((c % 2) == 0)
+d += c;
+}
+printf("%ld\n", d);
+return (0);
 }
