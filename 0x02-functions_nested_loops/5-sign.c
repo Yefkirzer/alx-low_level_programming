86506c80b1a1a6df5a6b5354bf219da0b6f7f4fa
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-22 12:04:19 -0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,25 @@
 #include "holberton.h"
-
 /**
- * main - check the code for ALX School students.
+ * print_sign - prints the sign of a number
+ * @n: n is an integer
  *
- * Return: Always 0.
+ * Return: 1 (if positive), 0 (if zero), -1 (if negative)
  */
-int main(void)
+int print_sign(int n)
 {
-  int r;
-
-  r = print_sign(98);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(0xff);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  r = print_sign(-1);
-  _putchar(',');
-  _putchar(' ');
-  _putchar(r + '0');
-  _putchar('\n');
-  return (0);
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
 }
