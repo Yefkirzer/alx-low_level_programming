2b0e53f8716e70d6ae9365617926260e6d0c1c70
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-22 11:48:03 -0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,19 +7,16 @@
  */
 int main(void)
 {
-  int i;
-  int z = 0;
-
-  while (i < 1024)
-    {
-      if ((i % 3 == 0) || (i % 5 == 0))
-	{
-	  z += i;
-	}
-
-      i++;
-    }
-
-  printf("%d\n", z);
-  return (0);
+int i;
+int z = 0;
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z += i;
+}
+i++;
+}
+printf("%d\n", z);
+return (0);
 }
