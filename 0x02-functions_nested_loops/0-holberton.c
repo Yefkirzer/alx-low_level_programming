81987724bf24cef091358d51b2e9717bfd63622a
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-22 09:08:22 -0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-x#include "holberton.h"
+#include "holberton.h"
 /**
  * main - prints Holberton
  *
@@ -18,24 +17,3 @@ int main(void)
   _putchar('\n');
 return (0);
 }
-=======
-#include "holberton.h"
-/**
- * main - prints Holberton
- *
- * Description: prints Holberton
- *
- * Return: 0 (Success)
- */
-int main(void)
-{
-char holberton[9] = "Holberton";
-int i;
-for (i = 0; i < 9; i++)
-{
-_putchar(holberton[i]);
-}
-_putchar('\n');
-return (0);
-}
->>>>>>> 25d01e98c0763063681cd8fa33e7f2f827e7cfec
