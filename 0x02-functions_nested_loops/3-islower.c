6211b1bfd8177f4a998bdf4c2a43ecd12a73dc77
Yefkirzer <yefkirzertesfaye73@gmail.com>
2021-06-22 07:08:10 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10x
+ *
+ * Return: void
+ */
+int _islower(int c)
+{
+  if ( islower(Ch) )
+    {
+      return(1);
+    }
+  else
+    {
+      return(0);
+    }  
+}
