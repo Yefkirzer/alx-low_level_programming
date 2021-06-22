d67015e497ed6dc7d063a2b4c1cc6e10efce1503
Yefkirzer <yefkirzertesfaye73@gmail.com>
2021-06-22 07:54:26 -0700

    bug fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,26 +7,26 @@
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1;
-	long k = 2;
+  int i = 0;
+  long j = 1;
+  long k = 2;
 
-	while (i < 50)
+  while (i < 50)
+    {
+      if (i == 0)
+	printf("%ld", j);
+      else if (i == 1)
+	printf(", %ld", k);
+      else
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
-
-		++i;
+	  k += j;
+	  j = k - j;
+	  printf(", %ld", k);
 	}
 
-	printf("\n");
-	return (0);
+      ++i;
+    }
+
+  printf("\n");
+  return (0);
 }
