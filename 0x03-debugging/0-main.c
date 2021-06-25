e4d340680db9b044680469397df7837e171e44a5
Yefkirzer <yefkirzertesfaye73@gmail.com>
2021-06-24 23:14:03 -0700

    fix bug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - Test  function for positive or negative
+ * main - Test function for positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-int i;
+  int i;
 
-i = 0;
+  i = 0;
+  positive_or_negative(i);
 
-positive_or_negative(i);
-return (0);
+  return (0);
 }
