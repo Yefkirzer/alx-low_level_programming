47baac33b651c945328cb2e5c06e0ae63afdac11
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-28 12:14:17 -0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include "holberton.h"
+
+/**
+ * _strlen - calculate the length of a string
+ * @s: pointer of char
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+while (s[i])
+i++;
+
+return (i);
+}
