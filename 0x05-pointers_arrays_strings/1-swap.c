2aa7dc31697924b391a114f944a5c51dc5e51724
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-28 12:13:28 -0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer of int
+ * @b: pointer of int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
