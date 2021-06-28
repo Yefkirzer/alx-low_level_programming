3728760c4bd00a76c7104ef61e617ca3a9973f2c
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-06-28 12:19:52 -0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string,in reverse , followed by a new line
+ * @s: pointer of char
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i = 0;
+
+while (s[i])
+i++;
+while (i--)
+_putchar(s[i]);
+_putchar('\n');
+}
