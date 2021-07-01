a97167d32768fb80264d33f9b422d3fbc41fe490
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-07-01 10:40:49 -0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * _strncat - function
  *@dest: char
@@ -7,26 +6,21 @@
  *@n: int
  *Return: char
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	i = 0;
-
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	j = 0;
-	while (src[j] != '\0' && j < n)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
+int i, j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
