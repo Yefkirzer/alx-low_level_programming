a7f423760cef4b283fada11fb8fa4e92393bd02c
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-07-05 11:33:53 -0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,14 @@
+  
 #include "holberton.h"
-#include <stdio.h>
 /**
- * set_string - sets the value of a pointer to a char
- * @s: double pointer
- * @to: pointer
- *
- * Return : Void function .
- */
+ * set_string - this function set a pointer to pointer
+ * @s: the pointer
+ * @to: the other pointer
+ (* a blank line
+* Description: this function set a pointer)?
+(* section header: the header of this function is holberton.h)*
+* Return: no return
+*/
 void set_string(char **s, char *to)
 {
 *s = to;
