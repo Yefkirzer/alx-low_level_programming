c948a1c0ed916cde340a931a1e06c1a7a5e9431f
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-07-07 11:36:32 -0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - print revesed string .
+ * @s: string to print .
+ */
+void _print_rev_recursion(char *s)
+{
+if (!*s)
+return;
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
