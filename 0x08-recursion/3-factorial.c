e50270a2d3e461f90e3c997090335f0076435be5
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-07-07 11:39:59 -0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include "holberton.h"
+
+/**
+ * factorial - compute the factorial of a given number.
+ * @n: the number
+ * Return: factorial number
+ */
+int factorial(int n)
+{
+int fac = 1;
+if (n < 0)
+return (-1);
+else if (!n)
+return (1);
+fac = factorial(n - 1);
+return (fac * n);
+}
