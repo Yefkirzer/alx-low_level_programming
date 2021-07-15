e28eb794a355d21b04adae2038b0c0113970b2cd
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-07-15 12:12:29 -0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,15 +15,18 @@ while (n--)
 *s++ = b;
 return (ptr);
 }
+
 /**
  * *_calloc - allocates memory for an array, using malloc
  * @nmemb: array length
  * @size: size of each element
  * Return: pointer
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *m;
+
 if (size == 0 || nmemb == 0)
 return (NULL);
 m = malloc(nmemb * size);
