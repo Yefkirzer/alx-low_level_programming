ba70d6205ba2d9a565d28b6dc3b769f1c6447ad6
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-07-19 11:10:45 -0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+  #include <stdio.h>
+
+/**
+ * main - Print the name of the file the program was compiled from
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
