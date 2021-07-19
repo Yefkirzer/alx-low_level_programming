b3025d9d847716a03ca6eca35917628edf5c390d
Yefkirzer <67009128+Yefkirzer@users.noreply.github.com>
2021-07-19 11:28:11 -0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dog struct memory
+ * @d: dog to free
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->owner);
+free(d->name);
+free(d);
+}
+}
